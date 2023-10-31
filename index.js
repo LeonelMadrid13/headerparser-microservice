087c3446af1f7f3b399566d01d1a2329d232dbf7
MasterKiri13 <leoneleduardomadrid@hotmail.com>
2023-10-30 18:45:39 -0600

    [Fix] Fixing the bug in the index.js file

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/api/whoami', (req, res) => {
   const ipaddress = req.ip;
   const language = req.headers['accept-language'];
   const software = req.headers['user-agent'];
-  res.json({ ipaddress, language, software });
+  res.json({ "ipaddress": ipaddress, "language": language, "software": software });
 });
 
 // listen for requests :)
